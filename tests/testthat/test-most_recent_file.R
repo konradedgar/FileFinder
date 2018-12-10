0b45617558279193266f0cb0d872973069dc4538
Konrad Zdeb <konrad.zdeb@me.com>
2018-12-10 17:54:31 +0000

    added test
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- tests/testthat/test-most_recent_file.R
+++ tests/testthat/test-most_recent_file.R
@@ -5,3 +5,10 @@ test_that(
     code = expect_error(object = most_recent_file(path = "wrong path"),
                         regexp = "Assertion on \\'path\\' failed")
 )
+
+test_that(desc = "vector is of correct length",
+          code = expect_length(object = most_recent_file(
+              path = Sys.getenv("R_DOC_DIR"),
+              n = 1
+          ),
+          n = 1))

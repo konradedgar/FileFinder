19ccf17c45bb58af469b4ff5b1f2868d0d8c7bd6
Konrad Zdeb <konrad.zdeb@me.com>
2018-12-11 07:42:32 +0000

    Added main package documentation

---
+++ R/FileFinder-package.R
@@ -0,0 +1,8 @@
+#' FileFinder: simply finding files
+#' 
+#' FileFinder provides a set of basic functions useful when finding files of
+#' specific characteristics like most recent modification or biggest / smallest
+#  size.
+#'
+#' @keywords internal
+"_PACKAGE"
